6de7f6c461c4299f402c07407cc34a5442783056
S-furi <stefano.furi@studio.unibo.it>
2024-03-14 18:51:19 +0100

    chore: add tail recursion check annotation

--- src/main/scala/exercises/Ex6.scala
+++ src/main/scala/exercises/Ex6.scala
@@ -1,6 +1,9 @@
 package exercises
 
+import scala.annotation.tailrec
+
 object Ex6 extends App {
+    @tailrec
     def gcd(a: Int, b: Int): Int = (a, b) match
         case (a, b) if a == 0 || b == 0 => a
         case (a, b) if a > b => gcd(b, a % b)
