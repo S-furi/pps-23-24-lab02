ec507c7370d60145ca222b69e18370df5989dfde
S-furi <stefano.furi@studio.unibo.it>
2024-03-14 18:26:14 +0100

    feat: complete ex4

---
+++ src/main/scala/exercises/Ex4.scala
@@ -0,0 +1,13 @@
+package exercises
+
+object Ex4 {
+    val predicate: (Int, Int, Int) => Boolean = (x, y, z) => z == y && x <= y
+
+    val p1: Int => Int => Int => Boolean = x => y => z => predicate(x, y, z)
+
+    val p2: (Int, Int, Int) => Boolean = (x, y, z) => predicate(x, y, z)
+
+    def p3(x: Int)(y: Int)(z: Int): Boolean = predicate(x, y, z)
+
+    def p4(x: Int, y: Int, z: Int): Boolean = predicate(x, y, z)
+}
