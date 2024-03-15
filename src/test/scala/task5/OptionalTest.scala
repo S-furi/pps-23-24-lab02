e47dfe2706fe71187f6f7b79f14dd3d85f9fde02
S-furi <stefano.furi@studio.unibo.it>
2024-03-15 18:26:35 +0100

    feat: implement `filter`

--- src/test/scala/task5/OptionalTest.scala
+++ src/test/scala/task5/OptionalTest.scala
@@ -28,20 +28,44 @@ class OptionalTest:
   /** Task 5 -- Look the behaviour of map operator */
   @Test def mapShouldReturnEmptyWhenEmpty(): Unit = {
     val empty: Optional[Int] = Optional.Empty()
-    val result = Optional.map(empty, _ + 1)
+    val result = Optional.map(empty)(_ + 1)
     assertTrue(Optional.isEmpty(result))
   }
 
   @Test def mapShouldReturnTransformedValueWhenNonEmpty(): Unit = {
     val nonEmpty = Optional.Maybe(0)
-    val result = Optional.map(nonEmpty, _ + 1)
+    val result = Optional.map(nonEmpty)( _ + 1)
     assertEquals(1, Optional.orElse(result, 1))
   }
 
   @Test def mapShouldApplyTypesTransformation(): Unit = {
     val opt = Optional.Maybe("foo")
-    val result = Optional.map(opt, _.length())
+    val result = Optional.map(opt)(_.length())
     val expectedLength = "foo".length()
-
     assertEquals(expectedLength, Optional.orElse(result, expectedLength))
+  }
+
+  @Test def filterOnEmptyShouldReturnEmpty(): Unit = {
+    val predicate: Int => Boolean = _ > 0
+    val empty = Optional.Empty()
+    val filteredValue = Optional.filter(empty)(predicate)
+    assertTrue(Optional.isEmpty(filteredValue))
+  }
+
+  @Test def filterTruePredicateOnNonEmptyShouldReturnNonEmpty(): Unit = {
+    val n = 5
+    val predicate: Int => Boolean = _ > 0
+    val nonEmpty = Optional.Maybe(n)
+    val filteredValue = Optional.filter(nonEmpty)(predicate)
+    assertFalse(Optional.isEmpty(filteredValue))
+    assertEquals(Optional.Maybe(n), filteredValue)
+  }
+
+  @Test def filterFalsePredicateOnNonEmptyShouldReturnEmpty(): Unit = {
+    val n = -1 
+    val predicate: Int => Boolean = _ > 0
+    val nonEmpty = Optional.Maybe(n)
+    val filteredValue = Optional.filter(nonEmpty)(predicate)
+    assertTrue(Optional.isEmpty(filteredValue))
+    assertEquals(Optional.Empty(), filteredValue)
   }
